18bf20f836f7b9c6a1c569d71019583b057c54e9
Amelia Hampton <hamptoam.dev@outlook.com>
2020-12-07 15:32:48 -0600

    Initial commit to this repo, animal game completed and in working condition.

---
+++ AnimalGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float speed = 10.0f;
+    public float xRange = 10;
+    public GameObject projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+   
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        //keeping player in screen bounds 
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launching projectile
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
+}
